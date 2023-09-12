84d7e7ef3db2c3c833fb2001acca5645b73e61c2
OnepIeCeIsRealllllllllll <144825492+OnepIeCeIsRealllllllllll@users.noreply.github.com>
2023-09-12 22:44:47 +0700

    Add files via upload

---
+++ [Pre] A+B.py
@@ -0,0 +1,17 @@
+# gam = [1, 3, 5, 2]
+# gam.sort(reverse = True)
+# print(gam)
+
+# def compare():
+#     largest = max(gam)
+#     print(largest)
+
+# compare()
+
+# dict = {'firstname' : 'john', 'lastname' : 'crena'}
+# print('firstname' in dict.keys())
+
+a = 5
+b = 10
+
+print(a / b)
