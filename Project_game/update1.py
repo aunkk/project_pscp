a1a0886d08021592b61020f54a5204c10ab12a18
OnepIeCeIsRealllllllllll <144825492+OnepIeCeIsRealllllllllll@users.noreply.github.com>
2023-10-13 01:00:10 +0700

    Update update1.py

--- Project_game/update1.py
+++ Project_game/update1.py
@@ -11,23 +11,19 @@ sys_font = pygame.font.SysFont("roboto", 80)
 
 background = pygame.image.load("im/bg-1.jpg").convert_alpha()
 text_score = sys_font.render("score", False, "Black")#ยังไม่เเสดงมา
-# bg_width = background.get_width()
-# bg_hight = background.get_height()
 
 ghost = pygame.image.load("im/12.1.png")
 ghost = pygame.transform.scale(ghost, (120,120))
-# ghostrect = ghost.get_rect(m = (80, 300))
 GHOST_X_POS = 200
-
-scroll = 0
+# scroll = 0
 
 run = True
 while run:
-    key = pygame.key.get_pressed()
-    if key[pygame.K_a] and scroll > 0:
-        scroll -= 5
-    if key[pygame.K_d]:
-        scroll += 5
+    # key = pygame.key.get_pressed()
+    # if key[pygame.K_a] and scroll > 0:
+    #     scroll -= 5
+    # if key[pygame.K_d]:
+    #     scroll += 5
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
