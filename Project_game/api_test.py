e85068b34d07ef67db01a9cc23ee41e7048e0205
aun.kk <121716689+aunkk@users.noreply.github.com>
2023-11-10 17:19:04 +0700

    Add files via upload

---
+++ Project_game/api_test.py
@@ -0,0 +1,12 @@
+import requests
+
+api_key = "e1c5932f6c96b34e1263878d1f8b7931"
+city = input("country : ")
+
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+
+# ดึงข้อมูลจาก API ที่เลือกไว้
+res = requests.get(url).json()
+weather = res['weather'][0]['main']
+
+# print(res)
